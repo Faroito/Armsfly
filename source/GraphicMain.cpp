f5bce8ef5e92a4c6f2c88d9fd4f684f271441052
Timothée Couble <timothee.couble@gmail.com>
2020-04-24 15:25:46 +0200

    Separate binaries

---
+++ source/GraphicMain.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Timothée Couble on 17/10/2019.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "Scene.hpp"
+
+int main(int argc, const char **argv) {
+    stbi_set_flip_vertically_on_load(true);
+    scene::Scene app;
+
+    app.start();
+    return 0;
+}
